9a4be0f75758ede0753d5df1b39788fda86272c5
SFERAS <rthyssen73@gmail.com>
2023-12-10 14:58:14 -0300

    println ready

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,6 +1,13 @@
 package main
 
-func ()  {
-	Print("Hello world!")
-	
+import "fmt"
+
+func main() {
+	var conferenceName = "Go conference"
+	const conferenceTieckets = 50
+	const remainingTieckets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking aplication")
+	fmt.Println("We have total", conferenceTieckets, "tieckets and", remainingTieckets, "are still available.")
+	fmt.Println("Hello world!")
 }
