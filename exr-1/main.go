a3494ef79296433b00eb8d61ff6cf2f78d4c51f5
SFERAS <rthyssen73@gmail.com>
2023-12-10 23:21:20 -0300

    prinf

--- exr-1/main.go
+++ exr-1/main.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	a := 42
-	fmt.Println(a)
+	// adams := 42
+	// fmt.Printf(`42 is a bunary, %b\n`, adams)
+	// fmt.Printf(`42 is a bunary, %v\n`, adams)
 
-	b, c, d, _, f := 0, 1, 2, 3, "pappines"
-	fmt.Println(b,c,d,f)
+	a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
 
+	fmt.Printf(`%b\t %#x\n`, a, a)
+	fmt.Printf(`%b\t %#x\n`, b, b)
+	fmt.Printf(`%b\t %#x\n`, c, c)
+	fmt.Printf(`%b\t %#x\n`, d, d)
+	fmt.Printf(`%b\t %#x\n`, e, e)
+	fmt.Printf(`%b\t %#x\n`, f, f)
 }
