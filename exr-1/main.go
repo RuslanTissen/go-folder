87919b6b6f770ee01babf0f1ada58aadb7f40ddd
SFERAS <rthyssen73@gmail.com>
2023-12-12 21:16:23 -0300

    functions

--- exr-1/main.go
+++ exr-1/main.go
@@ -2,17 +2,11 @@ package main
 
 import "fmt"
 
+	
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
-	adams := 42
-	fmt.Printf(`42 is a bunary, %b\n`, adams)
-	fmt.Printf(`42 is a bunary, %v\n`, adams)
-
-	a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
-
-	fmt.Printf(`%b\t %#x\n`, a, a)
-	fmt.Printf(`%b\t %#x\n`, b, b)
-	fmt.Printf(`%b\t %#x\n`, c, c)
-	fmt.Printf(`%b\t %#x\n`, d, d)
-	fmt.Printf(`%b\t %#x\n`, e, e)
-	fmt.Printf(`%b\t %#x\n`, f, f)
+	fmt.Println(add(42, 13))
 }
